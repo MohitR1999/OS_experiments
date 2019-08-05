ee985b3d6564a87dbafcea45962833c53984b6ad
Mohit Ranjan <mohitr1999@gmail.com>
2019-08-05 12:19:24 +0000

    A new file added

---
+++ equality.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int result(string s, int k) {
+    return 0;
+}
+
+int main() {
+    string s;
+    cin>>s;
+    int k;
+    cin>>k;
+    cout<<result(s,k);
+    return 0;
+}
